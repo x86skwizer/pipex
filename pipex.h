9a690603816fa8879cf3f19c45627a6c2873f836
yamrire <yamrire@student.1337.ma>
2022-06-25 06:37:59 +0100

    Separate parsing from execution

--- pipex.h
+++ pipex.h
@@ -6,12 +6,19 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 05:28:12 by yamrire           #+#    #+#             */
-/*   Updated: 2022/06/25 05:37:34 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/06/25 06:23:47 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include "libft.h"
+#include "ftprintf.h"
 
 typedef	struct Pipex Pipex;
 struct	Pipex
@@ -21,6 +28,9 @@ struct	Pipex
 	int fd_pip[2];
 	char **cmd_options;
 };
+
+char	**arrange_paths(char **envp);
+char	**get_cmd_options(char *argv, char **envp);
 
 
 #endif
