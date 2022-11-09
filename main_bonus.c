61242c66652aa281832666d9d8bc43e7107b0e1f
yamrire <yamrire@student.1337.ma>
2022-11-09 09:22:02 +0100

    here_doc algo

--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 00:00:56 by yamrire           #+#    #+#             */
-/*   Updated: 2022/11/07 07:57:40 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/11/09 09:10:08 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,20 @@ void free_double(char **pointer)
 	pointer = NULL;
 }
 
+int	open_heredoc(pipex *cmd, char *av)
+{
+	cmd->fd_infile = open("./here_doc", O_APPEND | O_CREAT | O_TRUNC, 0644);
+	cmd->fd_outfile = open(av, O_APPEND | O_CREAT | O_TRUNC, 0644);
+	if (cmd->fd_infile == -1)
+	{
+		perror("pipex");
+		return (1);
+	}
+	if (cmd->fd_outfile == -1)
+		handle_error(errno);
+	return (0);
+}
+
 int open_files(pipex *cmd, int ac, char **av)
 {
 	cmd->fd_infile = open(av[1], O_RDONLY);
@@ -68,63 +82,91 @@ int open_files(pipex *cmd, int ac, char **av)
 
 int main(int ac, char *av[], char **envp)
 {
-	pipex cmd;
-	int i;
-	int ret_filefd;
+	pipex	cmd;
+	int		i;
+	int		ret_filefd;
+	char	*here_doc;
+	char	*limiter;
 
-	cmd.nbr_cmd = ac - 3;
-	cmd.fd_tmp = -1;
-	cmd.pid = malloc(cmd.nbr_cmd * sizeof(pid_t));
-	cmd.paths = arrange_paths(envp);
-	cmd.cmd = malloc(sizeof(char *) * (cmd.nbr_cmd + 1));
-	i = 0;
-	while (i < cmd.nbr_cmd)
+	if (ac >= 5)
 	{
-		cmd.cmd[i] = ft_strdup(av[ac - cmd.nbr_cmd - 1 + i]);
-		i++;
-	}
-	cmd.cmd[i] = NULL;
-	ret_filefd = open_files(&cmd, ac, av);
-	if (pipe(cmd.fd_pip))
-		handle_error(errno);
-	if (ret_filefd != 1)
-		in_process(&cmd, av[2], &cmd.pid[0], envp);
-	i = 1;
-	if (cmd.nbr_cmd > 2)
-	{
-		while (i < cmd.nbr_cmd - 1)
+		if (ac == 6 && cmpstr(av[1], "here_doc"))
 		{
-			if (cmd.fd_tmp != -1) {
-				close(cmd.fd_tmp);
+			ret_filefd = open_heredoc(&cmd, av[5]);
+			limiter = ft_strdup(av[2]);
+			here_doc = ft_strdup("null");
+			while (!(cmpstr(limiter, here_doc)) && !(cmpstr(ft_strjoin(limiter, "\n"), here_doc)))
+			{
+				free(here_doc);
+				ft_printf("pipex here_doc> ");
+				here_doc = get_next_line(0);
+				write(cmd.fd_infile, here_doc, ft_strlen(here_doc));
 			}
-			cmd.fd_tmp = dup(cmd.fd_pip[0]);
-			if (close(cmd.fd_pip[1]) == -1)
-				handle_error(errno);
-			if (close(cmd.fd_pip[0]) == -1)
-				handle_error(errno);
-			if (pipe(cmd.fd_pip))
-				handle_error(errno);
-			cmd.pid[i] = fork();
-			if (cmd.pid[i] == -1)
-				handle_error(errno);
-			else if (cmd.pid[i] == 0)
-				mid_process(&cmd, cmd.cmd[i], envp);
-			i++;
+			cmd.nbr_cmd = 2;
+			cmd.cmd = malloc(sizeof(char *) * (cmd.nbr_cmd + 1));
+			cmd.cmd[0] = ft_strdup(av[3]);
+			cmd.cmd[1] = ft_strdup(av[4]);
+			cmd.cmd[2] = NULL;
 		}
+		else
+		{
+			cmd.nbr_cmd = ac - 3;
+			cmd.cmd = malloc(sizeof(char *) * (cmd.nbr_cmd + 1));
+			i = 0;
+			while (i < cmd.nbr_cmd)
+			{
+				cmd.cmd[i] = ft_strdup(av[ac - cmd.nbr_cmd - 1 + i]);
+				i++;
+			}
+			cmd.cmd[i] = NULL;
+			ret_filefd = open_files(&cmd, ac, av);
+		}
+		cmd.pid = malloc(cmd.nbr_cmd * sizeof(pid_t));
+		cmd.fd_tmp = -1;
+		cmd.paths = arrange_paths(envp);
+		if (pipe(cmd.fd_pip))
+			handle_error(errno);
+		if (ret_filefd != 1)
+			in_process(&cmd, cmd.cmd[0], &cmd.pid[0], envp);
+		i = 1;
+		if (cmd.nbr_cmd > 2)
+		{
+			while (i < cmd.nbr_cmd - 1)
+			{
+				if (cmd.fd_tmp != -1) {
+					close(cmd.fd_tmp);
+				}
+				cmd.fd_tmp = dup(cmd.fd_pip[0]);
+				if (close(cmd.fd_pip[1]) == -1)
+					handle_error(errno);
+				if (close(cmd.fd_pip[0]) == -1)
+					handle_error(errno);
+				if (pipe(cmd.fd_pip))
+					handle_error(errno);
+				cmd.pid[i] = fork();
+				if (cmd.pid[i] == -1)
+					handle_error(errno);
+				else if (cmd.pid[i] == 0)
+					mid_process(&cmd, cmd.cmd[i], envp);
+				i++;
+			}
+		}
+		if (cmd.fd_tmp != -1)
+			close(cmd.fd_tmp);
+		cmd.fd_tmp = dup(cmd.fd_pip[0]);
+		if (close(cmd.fd_pip[0]) == -1)
+			handle_error(errno);
+		if (close(cmd.fd_pip[1]) == -1)
+			handle_error(errno);
+		cmd.pid[i] = fork();
+		if (cmd.pid[i] == -1)
+			handle_error(errno);
+		else if (cmd.pid[i] == 0)
+			out_process(&cmd, cmd.cmd[i], envp);
+		else
+			parent_process(&cmd, ret_filefd);
 	}
-	if (cmd.fd_tmp != -1)
-		close(cmd.fd_tmp);
-	cmd.fd_tmp = dup(cmd.fd_pip[0]);
-	if (close(cmd.fd_pip[0]) == -1)
-		handle_error(errno);
-	if (close(cmd.fd_pip[1]) == -1)
-		handle_error(errno);
-	cmd.pid[i] = fork();
-	if (cmd.pid[i] == -1)
-		handle_error(errno);
-	else if (cmd.pid[i] == 0)
-		out_process(&cmd, cmd.cmd[i], envp);
 	else
-		parent_process(&cmd, ret_filefd);
+		ft_printf("ERROR : INCORRECT ARGUMENTS !\n");
 	return (0);
 }
