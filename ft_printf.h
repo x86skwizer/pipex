1c3bfa776ce22b54c1044ba5543d2d049c18350e
yamrire <yamrire@student.1337.ma>
2022-11-11 11:11:33 +0100

    norm

---
+++ ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ftprintf.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 14:03:27 by yamrire           #+#    #+#             */
+/*   Updated: 2022/11/11 09:45:15 by yamrire          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_putchar(char c);
+int	ft_putnbr(long n, int *len);
+int	ft_putstr(char *s);
+int	ft_printf(const char *str, ...);
+int	ft_nbr_base(unsigned long long n, int base, int which, int *len);
+
+#endif
